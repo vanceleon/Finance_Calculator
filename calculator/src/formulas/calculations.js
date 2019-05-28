6e2b4ff9f0bf634eb7603fb8000406138a6c052e
vanceleon <vanceleon44@gmail.com>
2019-05-27 21:45:58 -0400

    adding future value of annuity

--- calculator/src/formulas/calculations.js
+++ calculator/src/formulas/calculations.js
@@ -1,19 +1,14 @@
-// function roundTo(n, digits) {
-//     if (digits === undefined) {
-//       digits = 0;
-//     }
-
-//     var multiplicator = Math.pow(10, digits);
-//     n = parseFloat((n * multiplicator).toFixed(11));
-//     var test =(Math.round(n) / multiplicator);
-//     return +(test.toFixed(digits));
-//   }
 
 export const futureValue = (pv, int, yr, pmt) => {
-    const intConversion = (1+int/100)
-    const fv = 0; 
-    // if()
-    // pv*(Math.pow(intConversion,yr))
-    // console.log(fv)
+    const intConversion = (1 + int / 100)
+    let fv = 0;
+    if(pmt != 0) {
+
+    } 
+    fv = pv*(Math.pow(intConversion, yr))
     return fv;
+}
+
+export const futureValueOfAnnuity = (pv, int, yr, pmt) => {
+    
 }
