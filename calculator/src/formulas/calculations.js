6d769870dda060d8da26e638bdc67aff9f9bfe1e
vanceleon <vanceleon44@gmail.com>
2019-05-29 20:38:30 -0400

    refactoring for dry code edge case for pv and pmt

--- calculator/src/formulas/calculations.js
+++ calculator/src/formulas/calculations.js
@@ -3,11 +3,21 @@ export const futureValue = (pv, int, yr, pmt) => {
   let fv = 0;
   if (pmt !== 0 && pv === 0) {
     fv = futureValueOfAnnuity(pv, intConversion, yr, pmt);
-  } else {
+  } else if (pmt !== 0 && pv !== 0) {
+    fv = (futureValueOfAnnuity(pv, intConversion, yr, pmt)) + ;
+
+  }else {
     fv = pv * Math.pow(intConversion, yr);
   }
   return fv;
 };
+
+
+// creating separate function to keep dry code
+export const futureValueOfPresentValue = (pv, int, yr) {
+
+}
+
 
 export const futureValueOfAnnuity = (pv, int, yr, pmt) => {
   let fv = 0;
