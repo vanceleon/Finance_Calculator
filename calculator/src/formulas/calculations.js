9623e5a93c1004f8506da1556e6316a56dd8971f
vanceleon <vanceleon44@gmail.com>
2019-05-29 22:10:57 -0400

    fixed bug solve present value and annuity conversion to future value:

--- calculator/src/formulas/calculations.js
+++ calculator/src/formulas/calculations.js
@@ -1,10 +1,15 @@
 export const futureValue = (pv, int, yr, pmt) => {
-  const intConversion = 1 + int / 100;
+  const intConversion = ((1 + int) / 100) - 1;
+  console.log("intConversion",intConversion);
   let fv = 0;
   if (pmt !== 0 && pv === 0) {
     fv = futureValueOfAnnuity(pv, intConversion, yr, pmt);
   } else if (pmt !== 0 && pv !== 0) {
-    fv = futureValueOfPresentValue(pv, intConversion, yr) + (futureValueOfAnnuity(pv, intConversion, yr, pmt))
+    console.log(futureValueOfPresentValue(pv, intConversion, yr));
+    console.log(futureValueOfAnnuity(pv, intConversion, yr, pmt));
+    let annuity = futureValueOfAnnuity(pv, intConversion, yr, pmt);
+    let presentValue = futureValueOfPresentValue(pv, intConversion, yr);
+    fv =  presentValue + annuity;
     console.log("present value and annuity",fv);
 
   }else {
@@ -17,14 +22,16 @@ export const futureValue = (pv, int, yr, pmt) => {
 // creating separate function to keep dry code
 export const futureValueOfPresentValue = (pv, int, yr) => {
   let fv = 0;
-  fv = pv * Math.pow(int, yr);
-  console.log(fv)
+  fv = pv * Math.pow((1+int), yr);
+  // console.log(fv)
   return fv;
 }
 
 
 export const futureValueOfAnnuity = (pv, int, yr, pmt) => {
   let fv = 0;
-  fv = pmt * (((Math.pow(1 + int), yr) - 1) / int);
+  // console.log(Math.pow((onePlusInterest, yr)));
+  fv = pmt * (((Math.pow((1 + int), yr) - 1) / int));
+  // console.log(fv);
   return fv;
 };
