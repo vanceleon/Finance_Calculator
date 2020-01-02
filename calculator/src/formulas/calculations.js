a3972652be910719ca9ff59664b2e32a30ff689d
vanceleon <vanceleon44@gmail.com>
2020-01-02 16:07:55 -0500

    found bug in future value of annuity formula

--- calculator/src/formulas/calculations.js
+++ calculator/src/formulas/calculations.js
@@ -5,12 +5,12 @@ export const futureValue = (pv, int, yr, pmt) => {
   if (pmt !== 0 && pv === 0) {
     fv = futureValueOfAnnuity(pv, intConversion, yr, pmt);
   } else if (pmt !== 0 && pv !== 0) {
-    console.log(futureValueOfPresentValue(pv, intConversion, yr));
-    console.log(futureValueOfAnnuity(pv, intConversion, yr, pmt));
+    // console.log(futureValueOfPresentValue(pv, intConversion, yr));
+    // console.log(futureValueOfAnnuity(pv, intConversion, yr, pmt));
     let annuity = futureValueOfAnnuity(pv, intConversion, yr, pmt);
     let presentValue = futureValueOfPresentValue(pv, intConversion, yr);
     fv =  presentValue + annuity;
-    console.log("present value and annuity",fv);
+    // console.log("present value and annuity",fv);
 
   }else {
     fv = futureValueOfPresentValue(pv, intConversion, yr);
@@ -29,11 +29,13 @@ export const futureValueOfPresentValue = (pv, int, yr) => {
   return roundFV;
 }
 
-
+// formula needs to be corrected
 export const futureValueOfAnnuity = (pv, int, yr, pmt) => {
   let fv = 0;
+  let roundFV;
   // console.log(Math.pow((onePlusInterest, yr)));
   fv = pmt * (((Math.pow((1 + int), yr) - 1) / int));
   // console.log(fv);
-  return fv;
+  roundFV = fv.toFixed(2)
+  return roundFV;
 };
