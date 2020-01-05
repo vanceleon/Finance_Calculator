2d275b27130bae3a965fd5f3e74a0ede3fb6e4fa
vanceleon <vanceleon44@gmail.com>
2020-01-04 23:56:54 -0500

    fix formatting

--- calculator/src/formulas/waccCalculations.js
+++ calculator/src/formulas/waccCalculations.js
@@ -1,13 +1,12 @@
 export const weightedAverageCostOfCapital = (rf, beta, rm) => {
   const convertedRF = interestConversion(rf);
   const convertedRM = interestConversion(rm);
-  const costOfEquity = convertedRF + beta* (convertedRM - convertedRF)
+  const costOfEquity = convertedRF + beta * (convertedRM - convertedRF)
 
   return costOfEquity;
 }
 
 const interestConversion = interest => {
   const interestConversion = (interest / 100);
-
   return interestConversion;
 }
