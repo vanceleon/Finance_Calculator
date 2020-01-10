e7302ffc507bcb712b871b83557e60de4c3d06a0
vanceleon <vanceleon44@gmail.com>
2020-01-10 16:19:30 -0500

    calculating wacc worked out bugs using react hooks

--- calculator/src/Components/waccInputs.js
+++ calculator/src/Components/waccInputs.js
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
+import {weightedAverageCostOfCapital} from '../formulas/waccCalculations';
 
-const initialState = {
-  beta: 0,
-  rf: 0,
-  rm: 0,
-  wacc:0
-}
+// const initialState = {
+//   beta: 0,
+//   rf: 0,
+//   rm: 0,
+//   wacc:0
+// }
 
 export default function WACCInputs() {
-  const [input, setInput] = useState(initialState);
-
+  const [beta, setBeta] = useState(0);
+  const [rm, setRm] = useState(0);
+  const [rf, setRf] = useState(0);
+  const [wacc, setWacc] = useState(0);
+  
+  // const onChange = e => {
+  //   e.preventDefault();
+  //   setInput({inpute.target.name:e.target.value});
+  //   console.log(input)
+  // }
 
   const handleSubmit = e => {
     e.preventDefault();
-    
+    // console.log(e.target);
+    const wacc = weightedAverageCostOfCapital(rf, beta, rm)
+    console.log(wacc)
+    setWacc(wacc);
   }
 
   return (
@@ -22,31 +34,33 @@ export default function WACCInputs() {
           <span className='label'>Beta</span>
           <input
             className='present-value'
-            name='pv'
+            name='beta'
             type='number'
             placeholder='Beta'
-            // value={this.state.pv}
-            // onChange={thisonChange}
+            value={beta}
+            onChange={e => setBeta(e.target.value)}
           />
         </div>
         <div className='interest-rates'>
           <span className='label'>Risk Free Rate</span>
           <input
             className='interest-rate'
-            name='int'
+            name='rf'
             type='number'
             placeholder='Risk Free Rate'
+            value={rf}
+            onChange={e => setRf(e.target.value)}
           />
         </div>
         <div className='years'>
           <span className='label'>Market Return</span>
           <input
             className='term-length'
-            name='years'
+            name='rm'
             type='number'
             placeholder='Market Return'
-            // value={this.state.years}
-            // onChange={this.onChange}
+            value={rm}
+            onChange={e => setRm(e.target.value)}
           />
         <div className="result" onClick={e=> {
           handleSubmit(e)
@@ -55,7 +69,7 @@ export default function WACCInputs() {
         </div>
         </div>
         <div className="answer">
-          input.wacc
+          {wacc}
         </div>
 
       </form>
--- calculator/src/formulas/waccCalculations.js
+++ calculator/src/formulas/waccCalculations.js
@@ -1,7 +1,10 @@
 export const weightedAverageCostOfCapital = (rf, beta, rm) => {
   const convertedRF = interestConversion(rf);
   const convertedRM = interestConversion(rm);
+  console.log(convertedRF)
+  console.log(convertedRM)
   const costOfEquity = convertedRF + beta * (convertedRM - convertedRF)
+  console.log(beta)
 
   return costOfEquity;
 }
