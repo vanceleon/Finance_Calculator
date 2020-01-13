bd8f61c20b5c233b410d7bfbcaf1a0c59e438984
vanceleon <vanceleon44@gmail.com>
2020-01-13 09:20:49 -0500

    cost of debt equity and wacc functions setting obj for functions

--- src/formulas/waccCalculations.js
+++ src/formulas/waccCalculations.js
@@ -2,16 +2,30 @@ const interestConversion = interest => {
   const interestConversion = interest / 100;
   return interestConversion;
 };
+// need to import and assign values to this obj
+const WACCObj = {
+  weightOfDebt,
+  weightOfEquity,
+  rf,
+  beta,
+  rm,
+  int,
+  taxRate
+};
 
-export const weightedAverageCostOfCapital = (weightOfDebt, weightOfEquity) => {
-  // const weight
-  // const weightedCostOfDebt = weightOfDebt * costOfD;
+export const weightedAverageCostOfCapital = WACCObj => {
+  const costOfEq = costOfEquity(WACCObj.rf, WACCObj.beta, WACCObj.rm);
+  const costOfD = costOfDebt(WACCObj.int, WACCObj.taxRate);
+  const WACC =
+    costOfDebt * WACCObj.weightOfDebt + costOfEq * WACCObj.weightOfEquity;
+  return WACC;
 };
 
 const costOfEquity = (rf, beta, rm) => {
   const convertedRF = interestConversion(rf);
   const convertedRM = interestConversion(rm);
   const costOfEq = convertedRF + beta * (convertedRM - convertedRF);
+  return costOfEq;
 };
 
 export const costOfDebt = (int, taxRate) => {
