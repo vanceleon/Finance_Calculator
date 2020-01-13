60262db5c91fc85c070856af525151e960847569
vanceleon <vanceleon44@gmail.com>
2020-01-13 11:26:51 -0500

    formulas are working and bugs are worked out need to break out functions next for edge cases

--- src/Components/waccInputs.js
+++ src/Components/waccInputs.js
@@ -1,24 +1,27 @@
 import React, { useState } from 'react';
-import {weightedAverageCostOfCapital, costOfDebt, costOfEquity} from '../modules/waccCalculations';
+import {
+  weightedAverageCostOfCapital,
+  costOfDebt,
+  costOfEquity
+} from '../modules/waccCalculations';
 
 export default function WACCInputs() {
   // Cost of Equity
   const [beta, setBeta] = useState(0);
   const [rm, setRm] = useState(0);
   const [rf, setRf] = useState(0);
-  const [costOfEquity, setCostOfEquity] = useState(0);
-  
+  const [costEquity, setCostOfEquity] = useState(0);
+
   // Cost of Debt
   const [int, setInt] = useState(0);
   const [taxRate, setTaxRate] = useState(0);
-  const [costOfDebt, setCostOfDebt] = useState(0);
-  
+  const [costDebt, setCostOfDebt] = useState(0);
+
   // Weighted Average Cost of Capital
   const [weightEquity, setEquityWeight] = useState(0);
   const [weightDebt, setDebtWeight] = useState(0);
   const [wacc, setWACC] = useState(0);
-  
-  
+
   const handleSubmit = e => {
     e.preventDefault();
     const WACCObj = {
@@ -30,124 +33,131 @@ export default function WACCInputs() {
       int,
       taxRate
     };
-    if (WACCObj.weightOfDebt) {
+    console.log('I fired', WACCObj)
+    if (WACCObj.weightEquity) {
+      console.log('I fired')
       const wacc = weightedAverageCostOfCapital(WACCObj);
-      setWACC(wacc)
+      setWACC(wacc);
     } else if (WACCObj.rf) {
-      
+      const costofEq = costOfEquity(WACCObj);
+      setCostOfEquity(costofEq);
     } else {
-      
+      const costofD = costOfDebt(WACCObj);
+      setCostOfDebt(costofD);
     }
-  }
+  };
 
   return (
-      <form className='calculator-face'>
-        <h1>Cost of Equity</h1>
-        <div className='pv'>
-          <span className='label'>Beta</span>
-          <input
-            className='present-value'
-            name='beta'
-            type='number'
-            placeholder='Beta'
-            value={beta}
-            onChange={e => setBeta(e.target.value)}
-          />
-        </div>
-        <div className='interest-rates'>
-          <span className='label'>Risk Free Rate</span>
-          <input
-            className='interest-rate'
-            name='rf'
-            type='number'
-            placeholder='Risk Free Rate'
-            value={rf}
-            onChange={e => setRf(e.target.value)}
-          />
-        </div>
-        <div className='years'>
-          <span className='label'>Market Return</span>
-          <input
-            className='term-length'
-            name='rm'
-            type='number'
-            placeholder='Market Return'
-            value={rm}
-            onChange={e => setRm(e.target.value)}
-          />
-        <div className="result" onClick={e=> {
-          handleSubmit(e)
-        }}>
+    <form className='calculator-face'>
+      <h1>Cost of Equity</h1>
+      <div className='inputs'>
+        <span className='label'>Beta</span>
+        <input
+          className='present-value'
+          name='beta'
+          type='number'
+          placeholder='Beta'
+          value={beta}
+          onChange={e => setBeta(e.target.value)}
+        />
+      </div>
+      <div className='inputs'>
+        <span className='label'>Risk Free Rate</span>
+        <input
+          className='interest-rate'
+          name='rf'
+          type='number'
+          placeholder='Risk Free Rate'
+          value={rf}
+          onChange={e => setRf(e.target.value)}
+        />
+      </div>
+      <div className='result'>
+        <span className='label'>Market Return</span>
+        <input
+          className='term-length'
+          name='rm'
+          type='number'
+          placeholder='Market Return'
+          value={rm}
+          onChange={e => setRm(e.target.value)}
+        />
+        <div
+          className='result'
+          onClick={e => {
+            handleSubmit(e);
+          }}
+        >
           =
         </div>
-        </div>
-        <div className="answer">
-          {costOfEquity}
-        </div>
+      </div>
+      <div className='answer'>{costEquity}</div>
 
-        <h1>Cost of Debt</h1>
-        <div className='pv'>
-          <span className='label'>Interest Rate</span>
-          <input
-            className='present-value'
-            name='int'
-            type='number'
-            placeholder='Interest Rate'
-            value={int}
-            onChange={e => setInt(e.target.value)}
-          />
-        </div>
-        <div className='interest-rates'>
-          <span className='label'>Tax Rate</span>
-          <input
-            className='interest-rate'
-            name='taxRate'
-            type='number'
-            placeholder='Tax Rate'
-            value={taxRate}
-            onChange={e => setTaxRate(e.target.value)}
-          />
-        </div>
- 
-        <div className="result" onClick={e=> {
-          handleSubmit(e)
-        }}>
-          =
-        </div>
-        <div className="answer">
-          {costOfDebt}
-        </div>
-        <div className='pv'>
-          <span className='label'>Weight of Equity</span>
-          <input
-            className='present-value'
-            name='weightEquity'
-            type='number'
-            placeholder='Weight of Equity'
-            value={weightEquity}
-            onChange={e => setEquityWeight(e.target.value)}
-          />
-        </div>
-        <div className='interest-rates'>
-          <span className='label'>Weight of Debt</span>
-          <input
-            className='interest-rate'
-            name='weightDebt'
-            type='number'
-            placeholder='Weight of Debt'
-            value={weightDebt}
-            onChange={e => setDebtWeight(e.target.value)}
-          />
-        </div>
-        <div className="result" onClick={e=> {
-          handleSubmit(e)
-        }}>
-          Calculate Weighted Average Cost of Capital
-        </div>
-        <div className="answer">
-          {wacc}
-        </div>
-      </form>
-   
+      <h1>Cost of Debt</h1>
+      <div className='inputs'>
+        <span className='label'>Interest Rate</span>
+        <input
+          className='present-value'
+          name='int'
+          type='number'
+          placeholder='Interest Rate'
+          value={int}
+          onChange={e => setInt(e.target.value)}
+        />
+      </div>
+      <div className='inputs'>
+        <span className='label'>Tax Rate</span>
+        <input
+          className='interest-rate'
+          name='taxRate'
+          type='number'
+          placeholder='Tax Rate'
+          value={taxRate}
+          onChange={e => setTaxRate(e.target.value)}
+        />
+      </div>
+      <div
+        className='result'
+        onClick={e => {
+          handleSubmit(e);
+        }}
+      >
+        =
+      </div>
+      <div className='answer'>{costDebt}</div>
+
+      <h1>Weighted Average Cost of Capital</h1>
+      <div className='inputs'>
+        <span className='label'>Weight of Equity</span>
+        <input
+          className='present-value'
+          name='weightEquity'
+          type='number'
+          placeholder='Weight of Equity'
+          value={weightEquity}
+          onChange={e => setEquityWeight(e.target.value)}
+        />
+      </div>
+      <div className='inputs'>
+        <span className='label'>Weight of Debt</span>
+        <input
+          className='interest-rate'
+          name='weightDebt'
+          type='number'
+          placeholder='Weight of Debt'
+          value={weightDebt}
+          onChange={e => setDebtWeight(e.target.value)}
+        />
+      </div>
+      <div
+        className='result'
+        onClick={e => {
+          handleSubmit(e);
+        }}
+      >
+        Calculate Weighted Average Cost of Capital
+      </div>
+      <div className='answer'>{wacc}</div>
+    </form>
   );
 }
--- src/modules/waccCalculations.js
+++ src/modules/waccCalculations.js
@@ -6,20 +6,25 @@ const interestConversion = interest => {
 export const weightedAverageCostOfCapital = WACCObj => {
   const costOfEq = costOfEquity(WACCObj);
   const costOfD = costOfDebt(WACCObj);
-  const WACC =
-    costOfD * WACCObj.weightDebt + costOfEq * WACCObj.weightEquity;
-  return WACC;
+  console.log(costOfEq);
+  console.log(costOfD);
+  const wacc = costOfD * WACCObj.weightDebt + costOfEq * WACCObj.weightEquity;
+  const roundedWacc = wacc.toFixed(2);
+  return roundedWacc;
 };
 
 export const costOfEquity = WACCObj => {
   const convertedRF = interestConversion(WACCObj.rf);
   const convertedRM = interestConversion(WACCObj.rm);
   const costOfEq = convertedRF + WACCObj.beta * (convertedRM - convertedRF);
-  return costOfEq;
+  const roundedCostEquity = costOfEq.toFixed(2);
+  return roundedCostEquity;
 };
 
 export const costOfDebt = WACCObj => {
   const convertedInt = interestConversion(WACCObj.int);
-  const costOfD = convertedInt * (1 + WACCObj.taxRate);
-  return costOfD;
+  const convertedTaxRate = interestConversion(WACCObj.taxRate);
+  const costOfD = convertedInt * (1 - convertedTaxRate);
+  const roundedCostDebt = costOfD.toFixed(2);
+  return roundedCostDebt;
 };
