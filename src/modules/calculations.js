a57f28b280a4ed19c3f883e45a0a4ae119e69953
vanceleon <vanceleon44@gmail.com>
2020-01-16 08:29:51 -0500

    latest;

--- src/modules/calculations.js
+++ src/modules/calculations.js
@@ -37,18 +37,21 @@ export const futureValueOfAnnuity = (pv, int, yr, pmt) => {
   console.log("pv",pv);
   if(pv){
     // if pv is given needs to be a CF problem
-    const cashFlows = []
+    let cashFlows;
     let i = yr;
     while (i > 1) {
       if(i === yr) {
         const pv1 = parseInt(pv, 10); 
         const pmt1 = parseInt(pmt, 10); 
         const initialCashFlow = pv1 + pmt1;        
-        const cF1 = initialCashFlow * Math.pow((1+int), i);
-        cashFlows.push(cF1);
-      }      
-      let cfPayments = pmt * Math.pow((1+int), i);
-      cashFlows.push(cfPayments);
+        const cf = initialCashFlow * Math.pow((1+int), i);
+        cashFlows += cf;
+        console.log(cashFlows)
+      }else if (i < yr) {
+        cashFlows += pmt * Math.pow((1+int), i);
+        // cashFlows.push(cfPayments);
+
+      }   
       i--;
     }
     console.log(cashFlows);
