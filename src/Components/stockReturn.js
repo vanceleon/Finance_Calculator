687f2540483723a42b6a6a8379e3627f199cbe6b
vanceleon <vanceleon44@gmail.com>
2020-01-19 18:34:23 -0500

    latest stock return component

--- src/Components/stockReturn.js
+++ src/Components/stockReturn.js
@@ -5,8 +5,53 @@ export default function StockReturn() {
   const [endPrice, setendPrice] = useState(0);
 
   return (
-    <div className='return-container'>
-      this is where I will cal stock returns
-    </div>
+    <form className='calculator-face'>
+      <div className='problem'>
+        <h1>Cost of Equity</h1>
+        <div className='inputs'>
+          <span className='label'>Beta</span>
+          <input
+            className='present-value'
+            name='beta'
+            type='number'
+            placeholder='Beta'
+            value={beta}
+            onChange={e => setBeta(e.target.value)}
+          />
+        </div>
+        <div className='inputs'>
+          <span className='label'>Risk Free Rate</span>
+          <input
+            className='interest-rate'
+            name='rf'
+            type='number'
+            placeholder='Risk Free Rate'
+            value={rf}
+            onChange={e => setRf(e.target.value)}
+          />
+        </div>
+        <div className='inputs'>
+          <span className='label'>Market Return</span>
+          <input
+            className='term-length'
+            name='rm'
+            type='number'
+            placeholder='Market Return'
+            value={rm}
+            onChange={e => setRm(e.target.value)}
+          />
+        </div>
+        <div className='result'>
+          <Button
+            onClick={e => {
+              handleEquitySubmit(e);
+            }}
+          >
+            Calculate
+          </Button>
+          <div className='calculated-result'>{costEquity}</div>
+        </div>
+      </div>
+    </form>
   );
 }
