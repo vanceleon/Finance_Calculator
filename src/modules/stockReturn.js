6f195ac6a87a0c71c95010433566be9ac953b388
vanceleon <vanceleon44@gmail.com>
2020-01-23 20:08:45 -0500

    conversion and result are calculated awaiting test

--- src/modules/stockReturn.js
+++ src/modules/stockReturn.js
@@ -1,4 +1,5 @@
-import {interestConversion} from './conversions/unitConversions';
+import { interestConversion } from './conversions/unitConversions';
+import StockReturn from '../Components/stockReturn';
 
 // export const stockHPRCalculation = stockObj => {
 //   stockObj.result = 0;
@@ -7,5 +8,9 @@ import {interestConversion} from './conversions/unitConversions';
 // }
 
 export const stockHPRCalculation = stockObj => {
+  const begPrice = interestConversion(stockObj.begPrice);
+  const endPrice = interestConversion(stockObj.endPrice);
+  const result = (endPrice - begPrice) / begPrice;
 
+  return result;
 };
