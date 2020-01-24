d0572dd4a30b545fff9976a231b4f0a8da4ef73d
vanceleon <vanceleon44@gmail.com>
2020-01-23 19:56:38 -0500

    setting the result function for stock return

--- src/modules/stockReturn.js
+++ src/modules/stockReturn.js
@@ -2,5 +2,6 @@ import {interestConversion} from './conversions/unitConversions';
 
 export const stockReturn = stockObj => {
   stockObj.result = 0;
-  
+  stockObj.beg
+  return stockObj.result;
 }
