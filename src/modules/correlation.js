786ba3c14bde54615c42497c7b348587085be8b0
vanceleon <vanceleon44@gmail.com>
2020-02-03 19:41:50 -0500

    starting stockCorrelationFunc in modules

---
+++ src/modules/correlation.js
@@ -0,0 +1,3 @@
+export const stockCorrelationFunc = (tickerObj) => {
+  // will need to hit backend endpoint here that will pull/scrap historical prices and calculate correlation between two stocks
+}
