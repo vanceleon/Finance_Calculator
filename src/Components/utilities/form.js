d3560309650c7c21ca1dea7b300a110a2d5779e3
vanceleon <vanceleon44@gmail.com>
2020-02-06 22:00:23 -0500

    created map for inputs

--- src/Components/utilities/form.js
+++ src/Components/utilities/form.js
@@ -1,8 +1,46 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Button } from 'semantic-ui-react';
 
-const initialState = {}
+// initialState will be an obj containing all input data
+// initialState = {
+//   header: Testing
+//   inputs:[{title: Title, value: Value}],
+//   result:
+// }
 
 export default function InputForm() {
-  const [inputObj, setinputObj] = useState(initialState)
+  const [inputObj, setinputObj] = useState(initialState);
+
+  return (
+    <form className='calculator-face'>
+      <div className='problem'>
+        <h1>{inputObj.header}</h1>
+        {inputObj.inputs.map((i) => {
+          return (
+            <div className='inputs'>
+              <span className='label'>{inputObj.inputs[i].title}</span>
+              <input
+                className='present-value'
+                name='begPrice'
+                type='number'
+                placeholder='Beta'
+                value={inputObj.inputs[i].value}
+                onChange={e => setinputObj(e.target.value)}
+              />
+            </div>
+          );
+        })}
+        <div className='result'>
+          <Button
+            onClick={e => {
+              handleSubmit(e);
+            }}
+          >
+            Calculate
+          </Button>
+          <div className='calculated-result'>{inputObj.result}</div>
+        </div>
+      </div>
+    </form>
+  );
 }
