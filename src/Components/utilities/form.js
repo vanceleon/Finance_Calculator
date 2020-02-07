5e2329e7d140ac898a9d14a0074b9a4c91753a1d
vanceleon <vanceleon44@gmail.com>
2020-02-06 21:42:30 -0500

    creating reusable form

---
+++ src/Components/utilities/form.js
@@ -0,0 +1,8 @@
+import React, {useState} from 'react';
+import { Button } from 'semantic-ui-react';
+
+const initialState = {}
+
+export default function InputForm() {
+  const [inputObj, setinputObj] = useState(initialState)
+}
