30d73ab0f2e1c1d7e3e0700c12dc31a2a259245b
vanceleon <vanceleon44@gmail.com>
2020-04-06 14:41:52 -0400

    editting correlation field

--- src/Components/correlation.js
+++ src/Components/correlation.js
@@ -18,43 +18,86 @@ export default function Correlation() {
     setResult(stockCorrelation);
   };
 
-  return (
-    <form className='calculator-face'>
-      <div className='problem'>
-        <h1>Correlation Between Two Stocks</h1>
-        <div className='inputs'>
-          <span className='label'>Enter Ticker Here</span>
-          <input
-            className='present-value'
-            name='begPrice'
-            type='number'
-            placeholder='Beta'
-            value={ticker1}
-            onChange={e => setticker1(e.target.value)}
-          />
+  if(result) {
+    return (
+      <form className='calculator-face'>
+        <div className='problem'>
+          <h1>Correlation Between Two Stocks</h1>
+          <div className='inputs'>
+            <span className='label'>Enter Ticker Here</span>
+            <input
+              className='present-value'
+              name='begPrice'
+              type='text'
+              placeholder={result}
+              value={ticker1}
+              onChange={e => setticker1(e.target.value)}
+            />
+          </div>
+          <div className='inputs'>
+            <span className='label'>Enter Ticker Here</span>
+            <input
+              className='interest-rate'
+              name='rf'
+              type='text'
+              placeholder={result}
+              value={ticker2}
+              onChange={e => setticker2(e.target.value)}
+            />
+          </div>
+          <div className='result'>
+            <Button
+              onClick={e => {
+                handleSubmit(e);
+              }}
+            >
+              Calculate
+            </Button>
+            <div className='calculated-result'>{result}</div>
+          </div>
         </div>
-        <div className='inputs'>
-          <span className='label'>Enter Ticker Here</span>
-          <input
-            className='interest-rate'
-            name='rf'
-            type='number'
-            placeholder='Risk Free Rate'
-            value={ticker2}
-            onChange={e => setticker2(e.target.value)}
-          />
+      </form>
+    )
+  }else{
+    
+    return (
+      <form className='calculator-face'>
+        <div className='problem'>
+          <h1>Correlation Between Two Stocks</h1>
+          <div className='inputs'>
+            <span className='label'>Enter Ticker Here</span>
+            <input
+              className='present-value'
+              name='begPrice'
+              type='text'
+              placeholder='Enter Ticker Here'
+              value={ticker1}
+              onChange={e => setticker1(e.target.value)}
+            />
+          </div>
+          <div className='inputs'>
+            <span className='label'>Enter Ticker Here</span>
+            <input
+              className='interest-rate'
+              name='rf'
+              type='text'
+              // placeholder={}
+              value={ticker2}
+              onChange={e => setticker2(e.target.value)}
+            />
+          </div>
+          <div className='result'>
+            <Button
+              onClick={e => {
+                handleSubmit(e);
+              }}
+            >
+              Calculate
+            </Button>
+            <div className='calculated-result'>{result}</div>
+          </div>
         </div>
-        <div className='result'>
-          <Button
-            onClick={e => {
-              handleSubmit(e);
-            }}
-          >
-            Calculate
-          </Button>
-          <div className='calculated-result'>{result}</div>
-        </div>
-      </div>
-    </form>
-  );
+      </form>
+    );
+  }
 }
