5347916b7909b741680d3186fc4417ba294eabf7
vanceleon <vanceleon44@gmail.com>
2020-04-14 23:45:36 -0400

    adding performance attribution calculation

---
+++ src/Components/performanceAttribution.js
@@ -0,0 +1,66 @@
+import React, {useState} from 'react';
+import { Button } from 'semantic-ui-react';
+
+export default function PerformanceAttribution() {
+  const [performanceResult, setperformanceResult] = useState(initialState)
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    // 
+    const performanceAttribution = {
+
+    };
+    // const  = stockCorrelationCalculation();
+    // console.log(stockCorrelation)
+    // setResult(stockCorrelation);
+  };
+  return(
+    <form className='calculator-face'>
+      <div className='problem'>
+        {/* <InputsInForm inputs={inputs} setInputs={setInputs} /> */}
+
+        <h1>Correlation Between Two Stocks</h1>
+        <div className='inputs'>
+          <span className='label'>Enter Ticker Here</span>
+          <input
+            className='input-value'
+            name='begPrice'
+            type='text'
+            value={ticker1}
+            onChange={e => setticker1(e.target.value)}
+          />
+        </div>
+        <div className='inputs'>
+          <span className='label'>Enter Ticker Here</span>
+          <input
+            className='input-value'
+            name='rf'
+            type='text'
+            value={ticker2}
+            onChange={e => setticker2(e.target.value)}
+          />
+        </div>
+        <div className='inputs'>
+          <span className='label'>Time Horizon</span>
+          <input
+            className='input-value'
+            name='rf'
+            type='text'
+            value={ticker2}
+            onChange={e => setticker2(e.target.value)}
+          />
+        </div>
+        <div className='result'>
+          <Button
+            onClick={e => {
+              handleSubmit(e);
+            }}
+          >
+            Calculate
+          </Button>
+          <div className='calculated-result'>{result}</div>
+        </div>
+      </div>
+    </form>
+  )
+}
