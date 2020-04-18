82d170d5d6863d0aca83a6f91fbffe979ca76463
vanceleon <vanceleon44@gmail.com>
2020-04-17 23:16:42 -0400

    price insertion function

---
+++ src/Components/performance/priceInsert.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import { Button } from 'semantic-ui-react';
+
+
+export default function SectorPerformance() {
+  const [stockReturn, setstockReturn] = useState(initialState)
+
+  return (
+    <React.Fragment>
+      Price insertion feature will go here to covert csv files to json and send it to calculation functions
+    </React.Fragment>
+  )
+}
