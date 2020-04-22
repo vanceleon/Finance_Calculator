af875baee18fc63192cb8d30d44e831feb29c795
vanceleon <vanceleon44@gmail.com>
2020-04-21 23:25:55 -0400

    creating data structure for portfolioSecurities

--- src/Components/performance/portfolioPerformance.js
+++ src/Components/performance/portfolioPerformance.js
@@ -1,13 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Button } from 'semantic-ui-react';
 
+const portfolioOfSecurities = [
+  { date: '4/21/2020', securities: { ticker: 'AAPL' } },
+  { date: '3/21/2020', securities: { ticker: 'AAPL' } },
+  { date: '2/21/2020', securities: { ticker: 'AAPL' } },
+  { date: '1/21/2020', securities: { ticker: 'AAPL' } },
+];
 
 export default function PortfolioPerformance() {
-  const [stockReturn, setstockReturn] = useState(initialState)
+  const [stockReturn, setstockReturn] = useState(initialState);
 
   return (
-    <React.Fragment>
-      this is where the users portfolio will go
-    </React.Fragment>
-  )
+  <React.Fragment>
+    will need to loop and add to it like a list
+  </React.Fragment>);
 }
