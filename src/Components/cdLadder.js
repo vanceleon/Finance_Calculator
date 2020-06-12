64dd4265bcb5e637fdbd286baa15a5804a43a9f9
vanceleon <vanceleon44@gmail.com>
2020-06-11 23:46:36 -0400

    adding cd ladder component

--- src/Components/cdLadder.js
+++ src/Components/cdLadder.js
@@ -1,44 +1,43 @@
 import React, { useState } from 'react';
 import { Button } from 'semantic-ui-react';
-import {stockCorrelationFunc} from '../modules/correlation';
 
-export default function Correlation() {
+export default function CDLadder() {
   const [result, setResult] = useState(0);
 
   const handleSubmit = e => {
     e.preventDefault();
-    const stockCorrelationObj = {
+    // const stockCorrelationObj = {
       
-    };
-    const stockCorrelation = stockCorrelationFunc(stockCorrelationObj);
-    setResult(stockCorrelation);
+    // };
+    // setResult();
   };
-
+// need a way to add or take away from the ladder and calculate the interest rates
   return (
     <form className='calculator-face'>
+      
       <div className='problem'>
         <h1>CD Ladder Calculator</h1>
         <div className='inputs'>
           <span className='label'>Enter Ticker Here</span>
-          <input
+          {/* <input
             className='present-value'
             name='begPrice'
             type='number'
             placeholder='Beta'
             value={ticker1}
             onChange={e => setticker1(e.target.value)}
-          />
+          /> */}
         </div>
         <div className='inputs'>
           <span className='label'>Enter Ticker Here</span>
-          <input
+          {/* <input
             className='interest-rate'
             name='rf'
             type='number'
             placeholder='Risk Free Rate'
             value={ticker2}
             onChange={e => setticker2(e.target.value)}
-          />
+          /> */}
         </div>
         <div className='result'>
           <Button
